6f68a3085eb872e87d184893c1b75c6cfa7bdf88
dengl <daniel.engleithner@htl-wels.at>
2025-10-27 15:02:24 +0100

    Initial commit

---
+++ train.py
@@ -0,0 +1,10 @@
+# Trainings Code
+
+from ultralytics import YOLO
+
+# Load a pretrained YOLO11n model
+model = YOLO("yolov8n.yaml")
+
+
+#Train the model
+results = model.train(data="config.yaml", epochs=30, imgsz=640, batch=16)
